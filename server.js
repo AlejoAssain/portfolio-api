5a17bc3f524233eb11b244bc0d662e1ca85139d3
Alejo <alejoassain00@gmail.com>
2022-04-12 20:06:01 -0300

    project data requests

--- server.js
+++ server.js
@@ -1,5 +1,40 @@
 const express = require("express");
 
+require("dotenv").config()
+
+
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+const port = process.env.port || 8000
 
 
+app.get("/projects", (req, res) => {
+  console.log(`Projects request. Language: ${req.body.lang}`)
+  if (["es", "en"].includes(req.body.lang)) {
+    let data = require(`./data/projectsModel`)
+    let projects = []
+
+    data.map(project => 
+      projects.push({
+        title: project.title,
+        description: (req.body.lang === "es") ? project.esDescription : project.enDescription,
+        url: project.url,
+        image: project.image
+      })
+    )
+
+    res.json(projects)
+  } else {
+    res.status(400).json("Enter a valid language... en / es")
+  }
+  
+})
+
+
+app.get("presentation")
+
+
+app.listen(port, () => {
+  console.log(`Server started on port: ${port}`)
+})
