65eeee1e6c4e036f661feafe61357c136478a775
Alejo <alejoassain00@gmail.com>
2022-04-13 19:23:41 -0300

    changes in data files

---
+++ data/projectsData.js
@@ -0,0 +1,25 @@
+const projectsData = [
+  {
+    title: "My Personal Blog",
+    esDescription: "Este es mi blog personal, en el que utilizo Flask. Acá implementé algunas tecnologías interesantes como autenticación, una BD con PostgreSQL y más",
+    enDescription: "none",
+    url: "https://alejo-blog.herokuapp.com/",
+    image: "python"
+  },
+  {
+    title: "Shopping Cart App",
+    esDescription: "Esta aplicación fue desarrollada con React.js y me sirvió para poner en práctica los hooks de React. Simula el funcionamiento de un carrito de compras",
+    enDescription: "none",
+    url: "https://alejo-shopping-cart.vercel.app/",
+    image: "react"
+  },
+  {
+    title: "Notes App",
+    esDescription: "Aplicación desarrollada con React.js. Me sirvió para practicar manejo de estado en React a traves de hooks",
+    enDescription: "none",
+    url: "https://alejo-notes-app.vercel.app/",
+    image: "react"
+  },
+]
+
+module.exports = projectsData
