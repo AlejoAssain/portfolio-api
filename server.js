c025e8371fa709907e249c65a2ecfda4d55a1d65
Alejo <alejoassain00@gmail.com>
2022-04-13 19:17:26 -0300

    ready for vercel

--- server.js
+++ server.js
@@ -9,13 +9,36 @@ app.use(express.urlencoded({ extended: true }))
 const port = process.env.port || 8000
 
 
+const projectsData = [
+  {
+    title: "My Personal Blog",
+    esDescription: "Este es mi blog personal, en el que utilizo Flask. Acá implementé algunas tecnologías interesantes como autenticación, una BD con PostgreSQL y más",
+    enDescription: "none",
+    url: "https://alejo-blog.herokuapp.com/",
+    image: "python"
+  },
+  {
+    title: "Shopping Cart App",
+    esDescription: "Esta aplicación fue desarrollada con React.js y me sirvió para poner en práctica los hooks de React. Simula el funcionamiento de un carrito de compras",
+    enDescription: "none",
+    url: "https://alejo-shopping-cart.vercel.app/",
+    image: "react"
+  },
+  {
+    title: "Notes App",
+    esDescription: "Aplicación desarrollada con React.js. Me sirvió para practicar manejo de estado en React a traves de hooks",
+    enDescription: "none",
+    url: "https://alejo-notes-app.vercel.app/",
+    image: "react"
+  },
+]
+
+
 app.get("/projects", (req, res) => {
   console.log(`Projects request. Language: ${req.body.lang}`)
   if (["es", "en"].includes(req.body.lang)) {
-    let data = require(`./data/projectsModel`)
     let projects = []
-
-    data.map(project => 
+    projectsData.map(project => 
       projects.push({
         title: project.title,
         description: (req.body.lang === "es") ? project.esDescription : project.enDescription,
