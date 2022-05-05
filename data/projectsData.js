0616b28319583583f119cd04465e705e798b9c24
Alejo <alejoassain00@gmail.com>
2022-05-05 18:00:37 -0300

    projects data changes

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,25 +1,29 @@
 const projectsData = [
   {
     title: "My Personal Blog",
-    esDescription: "Este es mi blog personal, en el que utilizo Flask. Acá implementé algunas tecnologías interesantes como autenticación, una BD con PostgreSQL y más",
-    enDescription: "none",
+    description: {
+      es: "Este es mi blog personal, en el que utilizo Flask. Acá implementé algunas tecnologías interesantes como autenticación, una BD con PostgreSQL y más",
+      en: "This is my personal blog, made with Flask. Here I implemented some interesting technologies, like authentication, PostgresSQL and more"},
     url: "https://alejo-blog.herokuapp.com/",
-    image: "python"
+    gitHubUrl: "https://github.com/AlejoAssain/personal_blog"
   },
   {
     title: "Shopping Cart App",
-    esDescription: "Esta aplicación fue desarrollada con React.js y me sirvió para poner en práctica los hooks de React. Simula el funcionamiento de un carrito de compras",
-    enDescription: "none",
+    description: {
+      es: "Esta aplicación fue desarrollada con React.js y me sirvió para poner en práctica los hooks de React. Simula el funcionamiento de un carrito de compras",
+      en: "This app was developed with React.js and it helped me to practice React hooks. It simulates a shopping cart functionality"},
     url: "https://alejo-shopping-cart.vercel.app/",
-    image: "react"
+    gitHubUrl: "https://github.com/AlejoAssain/shopping-cart-react"
   },
   {
-    title: "Notes App",
-    esDescription: "Aplicación desarrollada con React.js. Me sirvió para practicar manejo de estado en React a traves de hooks",
-    enDescription: "none",
-    url: "https://alejo-notes-app.vercel.app/",
-    image: "react"
+    title: "To Do List App",
+    description: { 
+      es: "Una aplicación desarrollada con React.js, que me sirvió para practicar manejo de estado en React a traves de Redux",
+      en: "An app developed with React.js that I made to practice the states management with Redux"},
+    url: "https://redux-practice-alejo.vercel.app/",
+    gitHubUrl: "https://github.com/AlejoAssain/redux-practice"
   },
 ]
+
 
 module.exports = projectsData
